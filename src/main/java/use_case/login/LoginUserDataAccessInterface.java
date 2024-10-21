19d11059e553164c10e640490c3af91415272733
thiyaan <thiyaan.karunanayake@mail.utoronto.ca>
2024-10-21 18:59:26 -0400

    Task 2.2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,5 +27,15 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user.
+     * @param name the user name
+     */
     void setCurrentUser(String name);
+
+    /**
+     * Gets the current user.
+     * @return blank
+     */
+    String getCurrentUser();
 }
